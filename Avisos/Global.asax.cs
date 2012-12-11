062b005fd41f4175d7edb4df0b9c16f936cb3348
Kevin Bolander <kevinbolander@gmail.com>
2012-12-10 20:31:33 -0600

    stuff commited

--- Avisos/Global.asax.cs
+++ Avisos/Global.asax.cs
@@ -25,7 +25,7 @@ namespace Avisos
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
 
     }
